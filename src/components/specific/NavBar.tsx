64764000ec059f0642c37ad2b8b8d4f3e76881cb
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-07-04 00:09:00 +0530

    style:navigation bar style fixed

--- src/components/specific/NavBar.tsx
+++ src/components/specific/NavBar.tsx
@@ -5,21 +5,22 @@ import { signOut } from 'next-auth/react';
 import { DarkModeIcon } from "../icons/DarkModeIcon";
 import { LightModeIcon } from "../icons/LightModeIcon";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const NavBar = () => {
   const { mode, toggleMode } = useTheme();
-
+  const pathname = usePathname();
   return (
-    <header className='mb-2 w-full bg-[#FAF9F6] dark:bg-[#282C35]'>
-      <nav className="w-full flex justify-center md:justify-end items-center py-4 px-6 gap-5">
-        <div className="flex items-center justify-center gap-4">
-          <Link href="/" className="font-semibold hover:underline">Home</Link>
-          <Link href="/dashboard" className="font-semibold hover:underline">Dashboard</Link>
+    <header className='mb-2 w-full bg-white dark:bg-black'>
+      <nav className="w-full flex justify-center md:justify-end items-center py-4 px-6 gap-5 md:gap-12">
+        <div className="flex items-center justify-center gap-4 md:gap-10">
+          <Link href="/" className={`font-semibold hover:underline ${pathname === '/' ? 'underline' : ''}`}>Home</Link>
+          <Link href="/dashboard" className={`font-semibold hover:underline ${pathname === '/dashboard' ? 'underline' : ''}`}>Dashboard</Link>
         </div>
-        <div className="flex items-center justify-end gap-2">
+        <div className="flex items-center justify-end gap-4 md:gap-6">
           <Button className="text-sm font-medium bg-red-600 hover:bg-red-800" variant="outline" onClick={() => signOut()}>Logout</Button>
           <div onClick={toggleMode} className="text-sm font-medium text-center">
-            {mode === "dark" ? <DarkModeIcon className="h-4 w-4" /> : <LightModeIcon className="h-4 w-4" />}
+            {mode === "dark" ? <DarkModeIcon className="h-6 w-6" /> : <LightModeIcon className="h-6 w-6" />}
           </div>
         </div>
       </nav>

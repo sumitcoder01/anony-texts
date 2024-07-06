470d6783a15e966fcca0c49d7c6cfc0e3f635818
Sumit <135352466+sumitcoder01@users.noreply.github.com>
2024-07-06 16:01:10 +0000

    refactor: code cleanup

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -26,7 +26,7 @@ export const authOptions: NextAuthOptions = {
             ],
           });
           if (!user) {
-            throw new Error('No user found with this email');
+            throw new Error('No user found with this email/username');
           }
           if (!user.isVerified) {
             throw new Error('Please verify your account before logging in');

a0e6cadb0228c2b463a4341aff629cc3f49fe80d
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-07-18 21:52:56 +0530

    add animations

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -9,6 +9,7 @@ import Autoplay from "embla-carousel-autoplay"
 import { useRef } from 'react';
 import messages from '@/constants/messages.json';
 import { EmailIcon } from "@/components/icons/EmailIcon";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 
 export default function Home() {
@@ -20,8 +21,8 @@ export default function Home() {
   return (
     <main className="bg-white dark:bg-black flex flex-col items-center md:mx-10 gap-10 rounded-md shadow-xl p-10 my-4">
       <section className="text-center mb-10 md:mb-12">
-        <h1 className="text-3xl md:text-4xl mb-10 font-bold">Dive into the World of Anonymous Texts</h1>
-        <p className="text-lg md:text-xl font-semibold"> Anony Texts - Where your identity remains a secret.</p>
+        <h1 className="text-3xl md:text-4xl mb-10 font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Dive into the World of Anonymous Texts</h1>
+        <TextGenerateEffect className="text-lg md:text-xl font-semibold" words={"Anony Texts - Where your identity remains a secret."} />
       </section>
       <Carousel
         plugins={[plugin.current]}
